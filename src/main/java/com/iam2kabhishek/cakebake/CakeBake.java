880b56350a84cdb0b5101ddb271eea7ee07f2154
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-28 22:55:05 +0530

    Add main class

---
+++ src/main/java/com/iam2kabhishek/cakebake/CakeBake.java
@@ -0,0 +1,22 @@
+package com.iam2kabhishek.cakebake;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class CakeBake extends Application {
+
+    private static Scene scene;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
