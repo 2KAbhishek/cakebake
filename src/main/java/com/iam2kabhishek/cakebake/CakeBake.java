9a77788eb144e2cfd5c592a11011270e7db7d91b
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-28 22:55:28 +0530

    Override start

--- src/main/java/com/iam2kabhishek/cakebake/CakeBake.java
+++ src/main/java/com/iam2kabhishek/cakebake/CakeBake.java
@@ -15,6 +15,19 @@ public class CakeBake extends Application {
 
     private static Scene scene;
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("cake"), 400, 400);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CakeBake.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
         launch();
     }
