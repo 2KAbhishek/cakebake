1888ca55e94253328032940eeae2f297c0b5776a
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-28 22:31:00 +0530

    Add instance vars

---
+++ src/main/java/com/iam2kabhishek/cakebake/Cake.java
@@ -0,0 +1,19 @@
+package com.iam2kabhishek.cakebake;
+
+import java.util.HashMap;
+
+public class Cake {
+
+    private final double priceSmall = 45.00;
+    private final double priceMedium = 65.00;
+    private final double priceBig = 80.00;
+
+    private HashMap<String, Double> prices;
+
+    private String size;
+    private int quantity;
+    private String name;
+    private String[] toppings;
+    private double totalPrice;
+
+}
