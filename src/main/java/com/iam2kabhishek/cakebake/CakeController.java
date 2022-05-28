a59902836c50f07404d1dd3119e067846dad9dac
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-28 22:57:40 +0530

    Initialize FXML components

--- src/main/java/com/iam2kabhishek/cakebake/CakeController.java
+++ src/main/java/com/iam2kabhishek/cakebake/CakeController.java
@@ -7,4 +7,28 @@ import java.util.ArrayList;
 import javafx.fxml.FXML;
 
 public class CakeController {
+    @FXML
+    private javafx.scene.control.TextField nameText;
+
+    @FXML
+    private javafx.scene.control.CheckBox lavaCheck;
+
+    @FXML
+    private javafx.scene.control.CheckBox butterCheck;
+
+    @FXML
+    private javafx.scene.control.CheckBox creamCheck;
+
+    @FXML
+    private javafx.scene.control.CheckBox fondueCheck;
+
+    @FXML
+    private javafx.scene.control.ChoiceBox<Integer> quantityCombo;
+
+    @FXML
+    private javafx.scene.control.ChoiceBox<String> sizeCombo;
+
+    @FXML
+    private javafx.scene.control.Label priceLabel;
+
 }
